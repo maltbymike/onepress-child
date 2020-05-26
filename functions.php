a9c6d4f3b7cc2db8a9dc185675180e9a68daf40a
Mike Mlatby <maltby.mike@gmail.com>
2020-05-25 22:41:33 -0400

    add gutenburg function

--- functions.php
+++ functions.php
@@ -11,3 +11,12 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
+
+function wplook_activate_gutenberg_products($can_edit, $post_type){
+	if($post_type == 'product'){
+		$can_edit = true;
+	}
+
+	return $can_edit;
+}
+add_filter('use_block_editor_for_post_type', 'wplook_activate_gutenberg_products', 10, 2);
