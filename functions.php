3456a4c89fd341bb47015079faba4def7070ab3f
Mike Maltby <mike@ingersollrentall.ca>
2021-01-11 20:14:54 -0500

    Add product search to header

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
 
     $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
@@ -11,3 +10,24 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+function ir_product_search_header() {
+  ?>
+  <div class="wp-block-woocommerce-product-search homepage-product-search container">
+    <div class="wc-block-product-search homepage-product-search container">
+      <form role="search" method="get" action="/">
+        <label for="wc-block-product-search-3" class="wc-block-product-search__label screen-reader-text">Search</label>
+        <div class="wc-block-product-search__fields">
+          <input type="search" id="wc-block-product-search-3" class="wc-block-product-search__field" placeholder="Find your next rental..." name="s"/>
+          <input type="hidden" name="post_type" value="product"/>
+          <button type="submit" class="wc-block-product-search__button" label="Search">
+            <svg aria-hidden="true" role="img" focusable="false" class="dashicon dashicons-arrow-right-alt2" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20"><path d="M6 15l5-5-5-5 1-2 7 7-7 7z"></path></svg>
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+  <?php
+}
+add_action( 'onepress_page_before_content', 'ir_product_search_header');
