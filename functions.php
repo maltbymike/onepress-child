45c5faa0b60daeec4e190a82a5fbf8fb6b225bb6
Mike <mike@ingersollrentall.ca>
2021-01-22 23:29:39 -0500

    back to filter

--- functions.php
+++ functions.php
@@ -81,12 +81,8 @@ add_action( 'woocommerce_before_shop_loop_item', 'ir_customize_single_upsells' )
 function ir_customize_single_upsells() {
    global $woocommerce_loop;
    if ( $woocommerce_loop['name'] == 'up-sells' ) {
-      // remove add to cart button
       remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
-      // add_filter( 'woocommerce_get_price_html', 'ir_change_product_price_rental_rates', 10, 2 );
-      remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
-      add_action( 'woocommerce_after_shop_loop_item_title', 'ir_template_loop_price_rental_rates', 10, 2 );
-
+      add_filter( 'woocommerce_get_price_html', 'ir_change_product_price_rental_rates', 10, 2 );
    }
 }
 
@@ -94,9 +90,9 @@ function ir_change_product_price_rental_rates( $price_html, $product ) {
 
   if ($product->is_type('simple_rental')) {
 
-    $price_html  = '<div class="rental-price 4-hour-rate">$' . $product->get_4_hour_rate() . '</div>';
-    $price_html .= '<div class="rental-price daily-rate">$' . $product->get_daily_rate() . '</div>';
-    $price_html .= '<div class="rental-price weekly-rate">$' . $product->get_weekly_rate() . '</div>';
+    $price_html  = '<span class="rental-price 4-hour-rate">$' . $product->get_4_hour_rate() . '</span>';
+    $price_html .= '<span class="rental-price daily-rate">$' . $product->get_daily_rate() . '</span>';
+    $price_html .= '<span class="rental-price weekly-rate">$' . $product->get_weekly_rate() . '</span>';
 
   }
 
