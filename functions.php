3dd38c7ecf8ae78146731545525fec1e173738d4
Mike <mike@ingersollrentall.ca>
2021-02-06 23:27:47 -0500

    change priority

--- functions.php
+++ functions.php
@@ -86,7 +86,7 @@ function ir_customize_single_upsells() {
    }
 }
 
-add_filter( 'woocommerce_get_price_html', 'ir_change_product_price_rental_rates', 10, 2 );
+add_filter( 'woocommerce_get_price_html', 'ir_change_product_price_rental_rates', 15, 2 );
 add_action( 'woocommerce_shop_loop_item_title', 'ir_add_wrapper_upsell_content', 5 );
 add_action( 'woocommerce_after_shop_loop_item_title', 'ir_close_wrapper_upsell_content', 15 );
 
