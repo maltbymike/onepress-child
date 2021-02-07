fac313ebc8db636df5739a67aab158847b38b4ec
Mike <mike@ingersollrentall.ca>
2021-02-06 23:24:23 -0500

    add rental rate price filter changes to all instances of price

--- functions.php
+++ functions.php
@@ -80,11 +80,16 @@ function ir_customize_single_upsells() {
    global $woocommerce_loop;
    if ( $woocommerce_loop['name'] == 'up-sells' ) {
       remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
-      add_filter( 'woocommerce_get_price_html', 'ir_change_product_price_rental_rates', 10, 2 );
-      add_action( 'woocommerce_shop_loop_item_title', 'ir_add_wrapper_upsell_content', 5 );
-      add_action( 'woocommerce_after_shop_loop_item_title', 'ir_close_wrapper_upsell_content', 15 );
+      // add_filter( 'woocommerce_get_price_html', 'ir_change_product_price_rental_rates', 10, 2 );
+      // add_action( 'woocommerce_shop_loop_item_title', 'ir_add_wrapper_upsell_content', 5 );
+      // add_action( 'woocommerce_after_shop_loop_item_title', 'ir_close_wrapper_upsell_content', 15 );
    }
 }
+
+add_filter( 'woocommerce_get_price_html', 'ir_change_product_price_rental_rates', 10, 2 );
+add_action( 'woocommerce_shop_loop_item_title', 'ir_add_wrapper_upsell_content', 5 );
+add_action( 'woocommerce_after_shop_loop_item_title', 'ir_close_wrapper_upsell_content', 15 );
+
 function ir_add_wrapper_upsell_content() {
   echo "<span class='rental-product-upsells-content'>";
 }
