bd8702f44606e1b32b3e30d785c00b3afa984545
Mike Maltby <mike@ingersollrentall.ca>
2021-03-13 23:47:20 -0500

    move social-profiles to a template file

--- functions.php
+++ functions.php
@@ -20,51 +20,54 @@ add_action( 'onepress_page_before_content', 'ir_product_search_header');
 
 // Override onepress_get_social_profiles to add noopener tag
 function onepress_get_social_profiles() {
-	$array = get_theme_mod( 'onepress_social_profiles' );
-	if ( is_string( $array ) ) {
-		$array = json_decode( $array, true );
-	}
-	$html = '';
-	if ( ! empty( $array ) && is_array( $array ) ) {
-		foreach ( $array as $k => $v ) {
-			$array[ $k ] = wp_parse_args(
-				$v,
-				array(
-					'network' => '',
-					'icon' => '',
-					'link' => '',
-				)
-			);
-
-			// Get/Set social icons
-			// If icon isset
-			$icons = array();
-			$array[ $k ]['icon'] = trim( $array[ $k ]['icon'] );
-
-			if ( $array[ $k ]['icon'] != '' && strpos( $array[ $k ]['icon'], 'fa' ) !== 0 ) {
-				$icons[ $array[ $k ]['icon'] ] = 'fa-' . $array[ $k ]['icon'];
-			} else {
-				$icons[ $array[ $k ]['icon'] ] = $array[ $k ]['icon'];
-			}
-
-			$network = ( $array[ $k ]['network'] ) ? sanitize_title( $array[ $k ]['network'] ) : false;
-			if ( $network && ! $array[ $k ]['icon'] ) {
-				$icons[ 'fa-' . $network ] = 'fa-' . $network;
-			}
-
-			$array[ $k ]['icon'] = join( ' ', $icons );
-
-		}
-	}
-
-	foreach ( (array) $array as $s ) {
-		if ( $s['icon'] != '' ) {
-			$html .= '<a target="_blank" rel="noopener" href="' . $s['link'] . '" title="' . esc_attr( $s['network'] ) . '"><i class="fa ' . esc_attr( $s['icon'] ) . '"></i></a>';
-    }
-		}
-
-	return $html;
+  get_template_part( 'templates/social', 'profiles' );
 }
+// function onepress_get_social_profiles() {
+// 	$array = get_theme_mod( 'onepress_social_profiles' );
+// 	if ( is_string( $array ) ) {
+// 		$array = json_decode( $array, true );
+// 	}
+// 	$html = '';
+// 	if ( ! empty( $array ) && is_array( $array ) ) {
+// 		foreach ( $array as $k => $v ) {
+// 			$array[ $k ] = wp_parse_args(
+// 				$v,
+// 				array(
+// 					'network' => '',
+// 					'icon' => '',
+// 					'link' => '',
+// 				)
+// 			);
+//
+// 			// Get/Set social icons
+// 			// If icon isset
+// 			$icons = array();
+// 			$array[ $k ]['icon'] = trim( $array[ $k ]['icon'] );
+//
+// 			if ( $array[ $k ]['icon'] != '' && strpos( $array[ $k ]['icon'], 'fa' ) !== 0 ) {
+// 				$icons[ $array[ $k ]['icon'] ] = 'fa-' . $array[ $k ]['icon'];
+// 			} else {
+// 				$icons[ $array[ $k ]['icon'] ] = $array[ $k ]['icon'];
+// 			}
+//
+// 			$network = ( $array[ $k ]['network'] ) ? sanitize_title( $array[ $k ]['network'] ) : false;
+// 			if ( $network && ! $array[ $k ]['icon'] ) {
+// 				$icons[ 'fa-' . $network ] = 'fa-' . $network;
+// 			}
+//
+// 			$array[ $k ]['icon'] = join( ' ', $icons );
+//
+// 		}
+// 	}
+//
+// 	foreach ( (array) $array as $s ) {
+// 		if ( $s['icon'] != '' ) {
+// 			$html .= '<a target="_blank" rel="noopener" href="' . $s['link'] . '" title="' . esc_attr( $s['network'] ) . '"><i class="fa ' . esc_attr( $s['icon'] ) . '"></i></a>';
+//     }
+// 		}
+//
+// 	return $html;
+// }
 
 /* Remove Categories from Single Products */
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
---
+++ templates/social-profiles.php
@@ -0,0 +1,47 @@
+<?php
+  $array = get_theme_mod( 'onepress_social_profiles' );
+  if ( is_string( $array ) ) {
+    $array = json_decode( $array, true );
+  }
+  $html = '';
+  if ( ! empty( $array ) && is_array( $array ) ) {
+    foreach ( $array as $k => $v ) {
+      $array[ $k ] = wp_parse_args(
+        $v,
+        array(
+          'network' => '',
+          'icon' => '',
+          'link' => '',
+        )
+      );
+
+      // Get/Set social icons
+      // If icon isset
+      $icons = array();
+      $array[ $k ]['icon'] = trim( $array[ $k ]['icon'] );
+
+      if ( $array[ $k ]['icon'] != '' && strpos( $array[ $k ]['icon'], 'fa' ) !== 0 ) {
+        $icons[ $array[ $k ]['icon'] ] = 'fa-' . $array[ $k ]['icon'];
+      } else {
+        $icons[ $array[ $k ]['icon'] ] = $array[ $k ]['icon'];
+      }
+
+      $network = ( $array[ $k ]['network'] ) ? sanitize_title( $array[ $k ]['network'] ) : false;
+      if ( $network && ! $array[ $k ]['icon'] ) {
+        $icons[ 'fa-' . $network ] = 'fa-' . $network;
+      }
+
+      $array[ $k ]['icon'] = join( ' ', $icons );
+
+    }
+  }
+
+  foreach ( (array) $array as $s ) {
+    if ( $s['icon'] != '' ) {
+      $html .= '<a target="_blank" rel="noopener" href="' . $s['link'] . '" title="' . esc_attr( $s['network'] ) . '"><i class="fa ' . esc_attr( $s['icon'] ) . '"></i></a>';
+    }
+    }
+
+  echo $html;
+
+?>
