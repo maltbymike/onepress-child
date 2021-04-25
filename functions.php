09912c64b7d9b5d4fd067b3e70e916973eaeda19
Mike Maltby <mike@ingersollrentall.ca>
2021-04-25 00:11:30 -0400

    query products

--- functions.php
+++ functions.php
@@ -146,7 +146,7 @@ add_filter ( 'loop_shop_columns', 'loop_columns', 999);
 // Add subcategory rate header below subcategories
 function ir_get_product_table( $category ) {
   get_template_part( 'templates/archive', 'producttable' );
-  $subcategory_products = new WP_Query( array( 'post_type' => 'simple_rental', 'product_cat' => $category->slug ) );
+  $subcategory_products = new WP_Query( array( 'post_type' => 'product', 'product_cat' => $category->slug ) );
     if($subcategory_products->have_posts()):?>
       <ul class="subcat-products">
           <?php while ( $subcategory_products->have_posts() ) : $subcategory_products->the_post(); ?>
