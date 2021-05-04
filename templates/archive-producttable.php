25803c62a8059fc4941c2c72803cc7add391519c
Mike Maltby <mike@ingersollrentall.ca>
2021-05-03 22:35:07 -0400

    try to call archive-producttable for subcategories

--- templates/archive-producttable.php
+++ templates/archive-producttable.php
@@ -108,26 +108,7 @@ $category_slug = $args['category_slug'];
         if ( $categories ) :
             foreach ( $categories as $category ) :
                 echo esc_html($category->name);
-                $products = new WP_Query( array(
-                    'post_type' => 'product',
-                    'tax_query' => array(
-                        array(
-                            'taxonomy' => 'product_cat',
-                            'field' => 'slug',
-                            'terms' => $category->slug,
-                        ),
-                    )
-                ) );
-                if ( $products->have_posts() ) :
-                ?>
-                    <ul>
-                        <?php while ( $products->have_posts() ) : $products->the_post(); ?>
-                            <li><?php the_title(); ?></li>
-                        <?php endwhile; ?>
-                    </ul>
-                <?php
-                    wp_reset_postdata();
-                endif;
+                get_template_part( 'templates/archive', 'producttable', array ( 'category_slug' => $category->slug ) );
             endforeach;
         endif;
         ?>
