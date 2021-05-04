4fe356c992d32dbbcd025fa6936167cf93376382
Mike Maltby <mike@ingersollrentall.ca>
2021-05-03 22:25:54 -0400

    test

--- templates/archive-producttable.php
+++ templates/archive-producttable.php
@@ -105,31 +105,31 @@ $category_slug = $args['category_slug'];
         $categories = get_terms(
             'product_cat', $args
         );
-        if ( $categories ) :
-            foreach ( $categories as $category ) :
-                echo esc_html($category->name);
-                $products = new WP_Query( array(
-                    'post_type' => 'product',
-                    'tax_query' => array(
-                        array(
-                            'taxonomy' => 'product_cat',
-                            'field' => 'slug',
-                            'terms' => $category->slug,
-                        ),
-                    )
-                ) );
-                if ( $products->have_posts() ) :
-                ?>
-                    <ul>
-                        <?php while ( $products->have_posts() ) : $products->the_post(); ?>
-                            <li><?php the_title(); ?></li>
-                        <?php endwhile; ?>
-                    </ul>
-                <?php
-                    wp_reset_postdata();
-                endif;
-            endforeach;
-        endif;
+        // if ( $categories ) :
+        //     foreach ( $categories as $category ) :
+        //         echo esc_html($category->name);
+        //         $products = new WP_Query( array(
+        //             'post_type' => 'product',
+        //             'tax_query' => array(
+        //                 array(
+        //                     'taxonomy' => 'product_cat',
+        //                     'field' => 'slug',
+        //                     'terms' => $category->slug,
+        //                 ),
+        //             )
+        //         ) );
+        //         if ( $products->have_posts() ) :
+        //         ?>
+        //             <ul>
+        //                 <?php while ( $products->have_posts() ) : $products->the_post(); ?>
+        //                     <li><?php the_title(); ?></li>
+        //                 <?php endwhile; ?>
+        //             </ul>
+        //         <?php
+        //             wp_reset_postdata();
+        //         endif;
+        //     endforeach;
+        // endif;
 
     </div>
 
