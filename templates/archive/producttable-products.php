c4609ccaf977d263c642a1104bc581b91225dca5
Mike Maltby <mike@ingersollrentall.ca>
2021-05-04 23:20:01 -0400

    remove the function from producttable-products.php

--- templates/archive/producttable-products.php
+++ templates/archive/producttable-products.php
@@ -72,26 +72,3 @@ if($subcategory_products->have_posts()):
     <?php endwhile;?>
 
 <?php endif; wp_reset_query(); ?>
-
-<?php
-function exclude_product_cat_children( $wp_query ) {
-
-    if( is_admin() ) {
-        return;
-    }
-
-    if( isset( $wp_query->query_vars['product_cat'] ) && $wp_query->is_main_query() ) {
-
-        $wp_query->set(
-            'tax_query', array( array (
-                'taxonomy' => 'product_cat',
-                'field' => 'slug',
-                'terms' => $wp_query->query_vars['product_cat'],
-                'include_children' => false
-            ) )
-        );
-
-    }
-}
-add_action('pre_get_posts', 'exclude_product_cat_children');
-?>
