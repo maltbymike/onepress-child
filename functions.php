ee1e0d8d645db08bebae59680999fa7e07e48892
Mike Maltby <mike@ingersollrentall.ca>
2021-06-09 23:31:12 -0400

    wrap product-table in a conditional to exclude main rental category

--- functions.php
+++ functions.php
@@ -153,17 +153,20 @@ function ir_template_loop_category_title_wrapper_open() {
 function ir_template_loop_category_title_wrapper_close() {
   echo '</div>';
 }
-remove_action( 'woocommerce_before_subcategory', 'woocommerce_template_loop_category_link_open', 10 );
-remove_action( 'woocommerce_after_subcategory', 'woocommerce_template_loop_category_link_close', 10 );
-remove_action( 'woocommerce_before_subcategory_title', 'woocommerce_subcategory_thumbnail', 10 );
 
-add_action( 'woocommerce_before_subcategory', 'ir_template_loop_category_title_wrapper_open', 10 );
-add_action( 'woocommerce_before_subcategory', 'ir_template_loop_category_link_open', 10 );
-// add_action( 'woocommerce_after_subcategory', 'ir_auto_subcategory_thumbnail_wrapper_open', 10 );
-// add_action( 'woocommerce_after_subcategory', 'ir_auto_subcategory_thumbnail', 10 );
-// add_action( 'woocommerce_after_subcategory', 'ir_auto_subcategory_thumbnail_wrapper_close', 10 );
-add_action( 'woocommerce_after_subcategory', 'ir_template_loop_category_link_close', 10 );
-add_action( 'woocommerce_after_subcategory', 'ir_template_loop_category_title_wrapper_close', 15);
+if is_product_category() && ! is_product_category( 'rental' ) {
+    remove_action( 'woocommerce_before_subcategory', 'woocommerce_template_loop_category_link_open', 10 );
+    remove_action( 'woocommerce_after_subcategory', 'woocommerce_template_loop_category_link_close', 10 );
+    remove_action( 'woocommerce_before_subcategory_title', 'woocommerce_subcategory_thumbnail', 10 );
+
+    add_action( 'woocommerce_before_subcategory', 'ir_template_loop_category_title_wrapper_open', 10 );
+    add_action( 'woocommerce_before_subcategory', 'ir_template_loop_category_link_open', 10 );
+    // add_action( 'woocommerce_after_subcategory', 'ir_auto_subcategory_thumbnail_wrapper_open', 10 );
+    // add_action( 'woocommerce_after_subcategory', 'ir_auto_subcategory_thumbnail', 10 );
+    // add_action( 'woocommerce_after_subcategory', 'ir_auto_subcategory_thumbnail_wrapper_close', 10 );
+    add_action( 'woocommerce_after_subcategory', 'ir_template_loop_category_link_close', 10 );
+    add_action( 'woocommerce_after_subcategory', 'ir_template_loop_category_title_wrapper_close', 15);
+}
 
 //Override Default setting for product # per row to force list view
 function loop_columns() {
